9e8f7a7e57e85adb0717af045f5edceb394e40ae
Charles Harvey <harvey.charles@gmail.com>
2018-06-26 13:52:56 +0200

    added warning lines to header and footer

--- webpack.config.js
+++ webpack.config.js
@@ -25,15 +25,15 @@ module.exports = {
     },
     devtool: 'source-map',
     module: {
-        loaders: [
-          {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            query: {
-              presets: ['es2015', 'stage-0']
-            }
-          }
-        ]
+        // loaders: [
+        //   {
+        //     test: /\.js$/,
+        //     exclude: /node_modules/,
+        //     loader: 'babel-loader',
+        //     query: {
+        //       presets: ['es2015', 'stage-0']
+        //     }
+        //   }
+        // ]
     }
 };
